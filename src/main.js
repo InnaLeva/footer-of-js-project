d430b056a210ec2ee3942e025a7522f92efed476
Inna Ratushna <Ratushnaya.uv@gmail.com>
2024-08-22 03:17:50 +0300

    create page

---
+++ src/main.js
@@ -0,0 +1,17 @@
+import {} from './js/header';
+
+import {} from './js/hero';
+
+import { createAboutInfoMarkup } from './js/about';
+
+import { createCardMarkup, handleCardClick } from './js/benefits';
+
+import {} from './js/projects';
+
+import { questions, ulElement, createFAQMarkup, styleElement } from './js/faq';
+
+import { createCoverCardMarkup, handleScroll } from './js/cover';
+
+import { fetchReviews, swiper } from './js/reviews';
+
+import {} from './js/footer';
